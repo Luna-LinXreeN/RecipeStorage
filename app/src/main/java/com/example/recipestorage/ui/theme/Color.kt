c394cb98f8ab01f3d96a52999e628888369d8d5c
Luna-LinXreeN <tobias.pachlatko@gmx.at>
2023-10-06 10:56:52 +0200

    updated colors

--- app/src/main/java/com/example/recipestorage/ui/theme/Color.kt
+++ app/src/main/java/com/example/recipestorage/ui/theme/Color.kt
@@ -3,7 +3,7 @@ package com.example.recipestorage.ui.theme
 import androidx.compose.ui.graphics.Color
 
 
-
+val Panache = Color(0xFFf1f9f1)
 val MossGreen = Color(0xFFb7dfb9)
 val DeYork = Color(0xFF94cf96)
 val Fern = Color(0xFF70bf73)
@@ -11,6 +11,8 @@ val FruitSalad = Color(0xFF4CAF50)
 val Goblin = Color(0xFF3d8c40)
 val Killarney = Color(0xFF2e6930)
 val Everglade = Color(0xFF1e4620)
+
+val LaserLemon = Color(0xFFFff65d)
 
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
